82fd18ebccffd035b333b0780211fd9b405e8f00
antunbarcot <antee365@gmail.com>
2026-03-25 16:54:01 +0100

    Napravljena jos jedna promjena

--- NewClass.cs
+++ NewClass.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,13 @@ namespace VJEZBA1
 {
     internal class NewClass
     {
+    }
+}
+
+public class NewClass_NOVAA
+{
+    public static void Hello()
+    {
+        Console.WriteLine("Nova klasa");
     }
 }
