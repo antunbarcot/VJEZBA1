d2b25328feaee84433678e13f6a98624b21d136c
antunbarcot <antee365@gmail.com>
2026-03-26 21:42:05 +0100

    Update Program.cs
    Krivo napisan kod

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Novo");
 Console.WriteLine("Novo2");
-Console-WriteLine("Jos jedna promjena");
+Console.WriteLine("Jos jedna promjena");
 //komentar
 //popravio bug
 
